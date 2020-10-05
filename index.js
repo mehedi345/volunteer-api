881dfbb43b4715e758ea35204ec030ccf5055a96
mehedi345 <mehedihasan5101220@gmail.com>
2020-10-05 01:58:38 -0700

    another api created

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-
+const ObjectId = require("mongodb").ObjectId;
 const importData = require('./data.json');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 let port = process.env.PORT || 5000;
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mehedi45:mehEDIui5634@cluster0.ozwps.mongodb.net/volunteerNetwork?retryWrites=true&w=majority";
 const app = express();
-
+const password = "mehEDIui5634"
 app.use(cors());
-
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('api created by author')
@@ -16,6 +22,25 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
         res.send(importData)
 })
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+
+client.connect(err => {
+  const collection = client.db("volunteerNetwork").collection("volunteers");
+
+  app.post('/addInfo', (req, res) => {
+    const newInfo = req.body;
+     collection.insertOne(newInfo)
+     .then(result => {
+         res.send(result.insertedCount > 0)
+     })
+  })
+
+   app.
+  
+});
 
 app.listen(port, () => {
     console.log(`Example listening on port ${port}`);
